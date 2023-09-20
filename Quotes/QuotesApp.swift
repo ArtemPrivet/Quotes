b726dfd01da98d38fe11e1d2a29e6f68740320ab
Artem Orlov <artem.o@investing.com>
2023-09-20 17:17:11 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Artem Orlov on 20.09.23.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
